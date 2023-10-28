79f2f1fe41f0e2c924737f2d5f5a144703a2020d
danieldferreira <danielferreiraadarme@gmail.com>
2023-10-28 17:37:42 -0400

    Calculator_JS_Theodinproject

---
+++ java.java
@@ -0,0 +1,24 @@
+let valueOne = ""
+let valueTwo = ""
+let operator = ""
+
+    document.addEventlistener("DOMcontentloaded", function(
+        // Saving html elements in JS
+        
+        let clear = document.writequerySelector(".clear")
+        let equal = document.writequerySelector(".equal")
+        let decimal = document.writequerySelector(".decimal")
+
+        let numbers = document.writequerySelector(".number")
+        let operators = document.writequerySelector(".operator")
+
+        let  = document.writequerySelector("")
+        let  = document.writequerySelector("")
+        
+
+
+
+
+
+
+    ))
